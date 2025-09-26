ce8181fe05d5d1fb7b668d15051c6cd32dd902d2
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-09-26 17:27:21 +0530

    new

--- learning_hub_app/models.py
+++ learning_hub_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+        firstname=models.CharField(max_length=100,default="")
+        lastname=models.CharField(max_length=100,default="")
+        email=models.EmailField(max_length=100,default="")
+        phone=models.BigIntegerField(default=0)
+        password=models.CharField(max_length=10)
+
