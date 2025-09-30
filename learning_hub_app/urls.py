6ce2eab638dd7f5ac2884f50d553f82e5272b089
BansiRaiyani43 <bansiraiyani9110@gmail.com>
2025-09-30 18:06:59 +0530

    new

--- learning_hub_app/urls.py
+++ learning_hub_app/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-# from .views import
+from .views import BASE
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-
-
+urlpatterns = [ 
+    path('home/',BASE,name='home'),
 
 
 
--- learning_hub_app/views.py
+++ learning_hub_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
+def BASE(request):
+    return render(request, 'home.html')
